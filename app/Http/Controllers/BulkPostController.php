23066c09749dbf3abf4ca43271b191e6cc229670
marketing.limited <info@marketing.limited>
2025-11-25 16:41:01 +0100

    fix: add missing namespace import for ApiResponse trait in BulkPostController

--- app/Http/Controllers/BulkPostController.php
+++ app/Http/Controllers/BulkPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Concerns\ApiResponse;
 use App\Services\BulkPostService;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
